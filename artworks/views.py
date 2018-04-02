f55e5c97f9f9add95d2b208e1a00473e346c3ab5
Oscar Acero <oeaceroat@gmail.com>
2018-04-01 22:08:04 -0500

    Se hacen ajustes a la lectura del archivo de resultados y a la visualizacion del grafo

--- artworks/views.py
+++ artworks/views.py
@@ -33,13 +33,14 @@ def index(request):
 
 def search_graph(request, title, person, place, year_ini, year_fin):
     response_data = {}
-    #    key = request.GET['key']
-    # response_data["data"] = xQuery_data(url_list, filter_option, keyword)
 
-    # execute_job(title, person, place, year_ini, year_fin)
+    # Ejecutar búsqueda
+    execute_job(title, person, place, year_ini, year_fin)
 
     # leer archivo
-    result_file = settings.BASE_DIR + '/muestraideal.txt'
+    result_file = settings.BASE_DIR + '/resultado.txt'
+
+    '''
     data_graph = leer_fichero_nodos(result_file)
 
     # armar grafo
@@ -62,14 +63,41 @@ def search_graph(request, title, person, place, year_ini, year_fin):
     print('Nodos: ', graph.number_of_nodes())
     print('Realaciones: ', graph.number_of_edges())
 
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(20, 10))
+    plt.title('Grafo completo')
 
+    # graficar y guardar grafo principal
     nx.draw_networkx(G=graph, node_size=100, font_size=8)
     url_graph = settings.BASE_DIR + '/artworks/static/artworks/grafo_pru.png'
     plt.savefig(url_graph, dpi=100)
     print(url_graph)
     plt.clf()
-    # graficar y guardar grafo
+    '''
+
+    #grafo 2
+
+    data_graph2 = leer_fichero_muestra(result_file)
+    nodes2 = data_graph2[0]
+    edges2 = data_graph2[1]
+
+    graph2 = nx.DiGraph()
+    graph2.add_nodes_from(nodes2)
+    graph2.add_edges_from(edges2)
+
+    print('Nodos: ', graph2.number_of_nodes())
+    print('Realaciones: ', graph2.number_of_edges())
+
+    plt.figure(figsize=(20, 10))
+    plt.title('Muestra del grafo')
+
+    # graficar y guardar grafo principal
+    nx.draw_networkx(G=graph2, node_size=100, font_size=10)
+    url_graph2 = settings.BASE_DIR + '/artworks/static/artworks/grafo2_pru.png'
+    plt.savefig(url_graph2, dpi=100)
+    print(url_graph2)
+    plt.clf()
+
+
 
 
     return JsonResponse(response_data)
@@ -77,7 +105,7 @@ def search_graph(request, title, person, place, year_ini, year_fin):
 
 def get_connection():
 
-    proxy_uri = "http://connect.virtual.uniandes.edu.co:22"
+    proxy_uri = 'http://connect.virtual.uniandes.edu.co:22'
     url = urlparse(proxy_uri)
 
     http_con = http.client.HTTPConnection(url.hostname, url.port)
@@ -89,8 +117,8 @@ def get_connection():
         headers['Proxy-Authorization'] = 'Basic ' + base64.b64encode(auth)
     '''
 
-    # host = 'bigdata-cluster1-01.virtual.uniandes.edu.co' # Cluster de 20 nodos
-    host = 'bigdata-cluster2-01.virtual.uniandes.edu.co' # Cluster de 4 nodos
+    host = 'bigdata-cluster1-01.virtual.uniandes.edu.co' # Cluster de 20 nodos
+    # host = 'bigdata-cluster2-01.virtual.uniandes.edu.co' # Cluster de 4 nodos
 
     user = 'bigdata20181001'
     password = 'd0947747a04e26b1b2d152278128d8d2'
@@ -103,41 +131,61 @@ def get_connection():
     ssh = paramiko.SSHClient()  # Iniciamos un cliente SSH
     ssh.load_system_host_keys()  # Agregamos el listado de host conocidos
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Si no encuentra el host, lo agrega automáticamente
-    ssh.connect(
-        hostname=host,
-        username=user,
-        password=password,
-        port=22,
-        sock=sock
-    )  # Iniciamos la conexión.
-
+    try:
+        ssh.connect(
+            hostname=host,
+            username=user,
+            password=password,
+            port=22,
+            sock=sock
+        )  # Iniciamos la conexión.
+    except Exception as e:
+        print(str(e))
     return ssh
 
 
 def search(ssh_obj, title, person, place, year_ini, year_fin):
 
-    command = f'(cd /home/bigdata20181001/prueba; ' \
-              f'spark-submit script_test.py {title} {person} {place} {year_ini} {year_fin})'
+    title = "'" + title + "'"
+    person = "'" + person + "'"
+    place = "'" + place + "'"
+    year_ini = "'" + year_ini + "'"
+    year_fin = "'" + year_fin + "'"
+
+    print('Parametros = ', title, '   ',  person, '   ',  place, '   ',  year_ini, '   ',  year_fin)
+
+    remove_file = '(cd /home/bigdata20181001; rm -R resultado.txt)'
+    command = f'(cd /home/bigdata20181001; ' \
+              f'spark-submit grafo.py {title} {person} {place} {year_ini} {year_fin})'
 
     # command_1 = f'(cd /home/bigdata20181001/prueba; ./shell_test.sh)'
     # command_2 = f'(cd /home/bigdata20181001/prueba; spark-submit script_test.py)'
 
+    # Eliminar archvio de resultados en la MEC
+    print('Borrando archivo de resultados en la MEC')
+    stdin, stdout, stderr = ssh_obj.exec_command(remove_file)
+    print("stderr: ", stderr.readlines())
+    print("pwd: ", stdout.readlines())
+
+    # Ejecutar búsqueda en el cluster
+    print('Éjecutando script en el cluster')
     stdin, stdout, stderr = ssh_obj.exec_command(command)
     print("stderr: ", stderr.readlines())
     print("pwd: ", stdout.readlines())
 
+    # Traer el el archivo de resultados
     scp = SCPClient(ssh_obj.get_transport())
     print('copiando archivo de respuesta')
-    scp.get(remote_path=f'/home/bigdata20181001/prueba/salida.txt')
+    scp.get(remote_path=f'/home/bigdata20181001/resultado.txt')
     print('archivo copiado')
+    ssh_obj.close()
 
-
-def execute_job(filter_option, keyword):
+def execute_job(title, person, place, year_ini, year_fin):
     print("Iniciamos la conexión")
     ssh_obj = get_connection()
     print("Realizamos búsqueda")
-    search(ssh_obj, filter_option, keyword)
-    ssh_obj.close()
+    search(ssh_obj,  title, person, place, year_ini, year_fin)
+
     print('Finalizado correctamente')
 
 
@@ -149,14 +197,56 @@ def leer_fichero_nodos(entrada):
     for linea in open(entrada, 'r'):
 
         linea = linea.strip()
-        origen, destino, tiporigen, tipodestino, fecha = linea.split('|')
+        try:
+            origen, destino, tiporigen, tipodestino, fecha, fecha2, nivel = linea.split('|')
+        except:
+            origen, destino, tiporigen, tipodestino, fecha, fecha2, nivel = linea.split('|')
+
 
         if origen not in nodos:
             nodos.append(origen)
 
-        if destino not in nodos:
+        if destino not in nodos and destino:
             nodos.append(destino)
 
         aristas.append((origen, destino))
+
+    return nodos, aristas
+
+
+def leer_fichero_muestra(entrada):
+
+    aristas_nivel1 = []
+    aristas_nivel2 = []
+    nodos = []
+    for linea in open(entrada, 'r'):
+        linea = linea.strip()
+        try:
+            origen, destino, tiporigen, tipodestino, fecha, fecha2, nivel = linea.split('|')
+        except:
+            origen, destino, tiporigen, tipodestino, fecha, fecha2, nivel = linea.split('|')
+
+        if len(aristas_nivel1)<20 and nivel=='1':
+
+            aristas_nivel1.append((origen, destino))
+            if origen not in nodos:
+                nodos.append(origen)
+            if destino not in nodos:
+                nodos.append(destino)
+
+        elif len(aristas_nivel2)<20 and nivel=='2':
+
+            aristas_nivel2.append((origen,destino))
+            if origen not in nodos:
+                nodos.append(origen)
+            if destino not in nodos:
+                nodos.append(destino)
+
+        if len(aristas_nivel1)==20 and len(aristas_nivel2)==20:
+            break
+
+    aristas = aristas_nivel1+aristas_nivel2  # lista aristas
+    print ('total aristas ', len(aristas))
+    print('total nodos ', len(nodos) )
 
     return nodos, aristas
